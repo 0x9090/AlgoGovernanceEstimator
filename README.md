545150fae5d7c57b3299f0e2bb9bd4931bdc9972
Nops <fake@email.com>
2022-01-06 00:29:45 -0500

    almost done

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # AlgoGovernanceEstimator
 
 Basically a glorified spreadsheet to estimate future Algo governance earnings.
+This will help you roughly estimate your returns if your parcipate in governance.
+The Price, Committed, and APY fields are editable, as those are the variables that may change.
+
+"Does your app track my balance?" - No. All calculations are done in Javascript, and never leave your browser.
 
 ### https://0x9090.github.io/AlgoGovernanceEstimator/
--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-let algoTable = document.getElementById("algoTable");
-
-let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-
-let observer = new MutationObserver(function(mutations) {
-     mutations.forEach(function(mutation) {
-         if (mutation.type === 'childList') {
-             refreshPrices();
-         }
-     });
-});
-
-observer.observe(list, {
-    attributes: true,
-    childList: true,
-    characterData: true,
-    subtree: true
-});
+//document.getElementById("priceInput").addEventListener("keyup", refreshPrices);
+document.querySelectorAll("table td").forEach(e => e.addEventListener("keyup", refreshPrices));
 
 function refreshPrices() {
-    console.log("working!");
+    let price = parseFloat(document.getElementById("priceInput").value);
+    let table = document.getElementById("tableData");
+    for (let i = 1; i < table.rows.length; i++) {
+        // row iteration
+        let row = table.rows[i];
+        console.log(row);
+        let committed = parseFloat(row.cells[2].innerText);
+        let apy = parseInt(row.cells[3].innerText);
+        let rewards = parseFloat(((committed * (apy / 100)) / 4).toFixed(6));
+        let balance = parseFloat((committed + rewards).toFixed(6));
+        let usd_value = parseFloat(((committed + rewards) * price).toFixed(2));
+        let formatted_usd_value = "$" + usd_value.toLocaleString("en-US");
+        row.cells[4].innerText = rewards;
+        row.cells[5].innerText = balance
+        row.cells[6].innerText = formatted_usd_value;
+        if (i >= table.rows.length) {
+            return;
+        }
+        console.log(i);
+        if (i !== row.rowIndex) {
+            let next_row = table.rows[i + 1];
+            next_row.cells[2].innerText = balance;
+        }
+    }
+}
+
+function resetPrices() {
+    let table = document.getElementById("tableData");
+    for (let i = 1; i < table.rows.length; i++) {
+        let row = table.rows[i];
+
+    }
 }
 
 function downloadCSV() {
+    console.log("downloading CSV");
     let csv_data = [];
     let rows = document.getElementsByTagName("tr");
     for (let i = 0; i < rows.length; i++) {
         let columns = rows[i].querySelectorAll("td,th");
         let csvrow = [];
-        for (let j = 0; j < cols.length; j++) {
+        for (let j = 0; j < columns.length; j++) {
             csvrow.push(columns[j].innerHTML);
         }
         csv_data.push(csvrow.join(","));
