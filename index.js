03d9632a93f9d4390cbee62dfc62d95fd5d4ba59
Nops <fake@email.com>
2022-01-05 22:57:33 -0500

    more styling

--- index.js
+++ index.js
@@ -20,3 +20,26 @@ observer.observe(list, {
 function refreshPrices() {
     console.log("working!");
 }
+
+function downloadCSV() {
+    let csv_data = [];
+    let rows = document.getElementsByTagName("tr");
+    for (let i = 0; i < rows.length; i++) {
+        let columns = rows[i].querySelectorAll("td,th");
+        let csvrow = [];
+        for (let j = 0; j < cols.length; j++) {
+            csvrow.push(columns[j].innerHTML);
+        }
+        csv_data.push(csvrow.join(","));
+    }
+    csv_data = csv_data.join("\n");
+    let CSVFile = new Blob([csv_data], { type: "text/csv" });
+    let temp_link = document.createElement("a");
+    temp_link.download = "AlgoEstimate.csv";
+    let url = window.URL.createObjectURL(CSVFile);
+    temp_link.href = url;
+    temp_link.style.display = "none";
+    document.body.appendChild(temp_link);
+    temp_link.click();
+    document.body.removeChild(temp_link);
+}
